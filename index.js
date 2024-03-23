fbc4a4b17f8b6e4e9106607c6e7a2a0732c52d5d
kennedy <kennedywakaba5@gmail.com>
2024-03-24 00:30:52 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `On Saturday, let's ${activity}!`;
+}
+function mondayWork(task = 'go to the office') {
+    return `On Monday, let's ${task}!`;
+}
+function wrapAdjective(flair = "*") {
+    return function(adjective = "a dedicated programmer") {
+        return `You are ${flair}${adjective}!`;
+    }
+}
